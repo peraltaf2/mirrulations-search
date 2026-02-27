18c0218ab8a73974e15b2416548d9c6dd8430da9
Deborah Rabinovich <rabinovichd@moravian.edu>
2026-02-27 15:57:55 -0500

    Add one docket

--- db/sample-data.sql
+++ db/sample-data.sql
@@ -1,1 +1,45 @@
 -- Sample data for dockets and document and cfrParts
+
+-- =========================================
+-- DOCKETS
+-- =========================================
+
+INSERT INTO dockets (
+    docket_id,
+    docket_api_link,
+    agency_id,
+    docket_category,
+    docket_type,
+    effective_date,
+    flex_field1,
+    flex_field2,
+    modify_date,
+    organization,
+    petition_nbr,
+    program,
+    rin,
+    short_title,
+    flex_subtype1,
+    flex_subtype2,
+    docket_title,
+    docket_abstract
+) VALUES (
+    'CMS-2025-0240',
+    'https://api.regulations.gov/v4/dockets/CMS-2025-0240',
+    'CMS',
+    NULL,
+    'Rulemaking',
+    NULL,
+    NULL,
+    NULL,
+    '2025-11-24T16:44:12Z',
+    NULL,
+    NULL,
+    NULL,
+    '0938-AV52',
+    NULL,
+    NULL,
+    NULL,
+    'CY 2026 Changes to the End-Stage Renal Disease (ESRD) Prospective Payment System and Quality Incentive Program.  CMS-1830-P',
+    'This proposed rule would update and revise the End-Stage Renal Disease (ESRD) Prospective Payment System for calendar year 2026.  This rule also proposes to update the payment rate for renal dialysis services furnished by an ESRD facility to individuals with acute kidney injury.  In addition, this rule proposes to update requirements for the ESRD Quality Incentive Program and to terminate and modify requirements for the ESRD Treatment Choices Model.'
+);
